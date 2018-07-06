9b37abfbb66171115e58b8d3c4d7618f9d634245
Jace Chan <jacetchan@gmail.com>
2018-07-06 16:45:41 -0700

    Deploy proxy

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require ('newrelic');
 const express = require('express');
 const path = require('path');
 // var cors = require('cors');
@@ -14,16 +15,16 @@ app.use('/listings/:listingId', express.static(path.join(__dirname, 'public')));
 // // booking module
 app.get('/listings/:listingId/booking*', (req, res) => {
   // console.log(req.url);
-  res.redirect(`http://ec2-52-53-170-87.us-west-1.compute.amazonaws.com${req.url}`);
+  res.redirect(`http://ec2-54-183-201-8.us-west-1.compute.amazonaws.com${req.url}`);
 });
 
 // description module
 app.get('/listings/:listingId/overview', (req, res) => {
-  res.redirect(`http://ec2-18-207-228-137.compute-1.amazonaws.com${req.url}`);
+  res.redirect(`http://ec2-52-23-163-170.compute-1.amazonaws.com${req.url}`);
 });
 
 app.get('/listings/:listingId/arrangements', (req, res) => {
-  res.redirect(`http://ec2-18-207-228-137.compute-1.amazonaws.com${req.url}`);
+  res.redirect(`http://ec2-52-23-163-170.compute-1.amazonaws.com${req.url}`);
 });
 
 // // reviews module
@@ -38,7 +39,7 @@ app.get('/listings/:listingId/arrangements', (req, res) => {
 // image module
 app.get('/listings/:listingId/pictures', (req, res) => {
   // console.log(req.url);
-  res.redirect(`http://ec2-35-170-203-196.compute-1.amazonaws.com${req.url}`);
+  res.redirect(`http://ec2-54-91-186-95.compute-1.amazonaws.com${req.url}`);
 });
 
 app.listen(port, () => {
